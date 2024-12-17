712494395607799c3ab8b20d6e39be7c0a1a4358
ayush-0012 <ayushbisht0012@gmail.com>
2024-12-17 14:06:12 +0530

    pattern15 done

--- patterns/pattern14.js
+++ patterns/pattern14.js
@@ -9,4 +9,4 @@ for(let i=1; i<=n; i++){
   }
 
   console.log(row);
-}
+} 
---
+++ patterns/pattern15.js
@@ -0,0 +1,11 @@
+let n = 5; 
+
+
+for(let i=0; i<n; i++){
+  let row = " "
+
+  for(let j=65; j<=65+(n-i-1); j++){
+    row += String.fromCharCode(j)
+  }
+console.log(row);
+}
